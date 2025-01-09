46facf6775331d1c917e574789b8e9ca5b794b94
Dmitry Romanenko <letisflywithme@icloud.com>
2025-01-09 12:06:13 +0300

    add import config to internal package

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,63 +1,7 @@
 package main
 
-import (
-	"context"
-	"github.com/RomanenkoDR/metrics/internal/config/agent"
-	"github.com/RomanenkoDR/metrics/internal/storage"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-)
+import "github.com/RomanenkoDR/metrics/internal/config/agent"
 
 func main() {
-	var cfg agent.Options
-	// Логируем старт приложения.
-	cfg, err := agent.ParseOptions()
-	if err != nil {
-		panic(err)
-	}
-
-	// Initiate new storage
-	m := storage.New()
-
-	// Init channels
-	done := make(chan struct{})
-	metricsCh := make(chan storage.MemStorage, cfg.RateLimit)
-	defer close(metricsCh)
-
-	// Collect data from MemStats and send to the server
-	// Gather facts
-	go func(timer time.Duration) {
-		for {
-			time.Sleep(timer)
-			agent.ReadMemStats(&m, metricsCh)
-		}
-	}(time.Second * time.Duration(cfg.PollInterval))
-
-	// Send metrics to the server
-	for w := 1; w <= cfg.RateLimit; w++ {
-		go func(timer time.Duration) {
-			for {
-				time.Sleep(timer)
-				fn := agent.Retry(agent.ProcessBatch, 3, 1*time.Second)
-				err := fn(context.Background(), cfg, metricsCh)
-				if err != nil {
-					log.Println(err)
-				}
-			}
-		}(time.Second * time.Duration(cfg.ReportInterval))
-	}
-
-	// Gracefull shutdown
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-		<-sigint
-
-		close(done)
-	}()
-
-	<-done
+	agent.RunAgent()
 }
---
+++ internal/config/agent/agent_run.go
@@ -0,0 +1,62 @@
+package agent
+
+import (
+	"context"
+	"github.com/RomanenkoDR/metrics/internal/storage"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func RunAgent() {
+	var cfg Options
+	// Логируем старт приложения.
+	cfg, err := ParseOptions()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initiate new storage
+	m := storage.New()
+
+	// Init channels
+	done := make(chan struct{})
+	metricsCh := make(chan storage.MemStorage, cfg.RateLimit)
+	defer close(metricsCh)
+
+	// Collect data from MemStats and send to the server
+	// Gather facts
+	go func(timer time.Duration) {
+		for {
+			time.Sleep(timer)
+			ReadMemStats(&m, metricsCh)
+		}
+	}(time.Second * time.Duration(cfg.PollInterval))
+
+	// Send metrics to the server
+	for w := 1; w <= cfg.RateLimit; w++ {
+		go func(timer time.Duration) {
+			for {
+				time.Sleep(timer)
+				fn := Retry(ProcessBatch, 3, 1*time.Second)
+				err := fn(context.Background(), cfg, metricsCh)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}(time.Second * time.Duration(cfg.ReportInterval))
+	}
+
+	// Gracefull shutdown
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+
+		close(done)
+	}()
+
+	<-done
+}
