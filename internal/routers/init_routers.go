e3a8c2f808533dc2f17abb48b12930d14ca6b2af
Dmitry Romanenko <letisflywithme@icloud.com>
2025-01-10 09:59:33 +0300

    update cycle import

--- internal/routers/init_routers.go
+++ internal/routers/init_routers.go
@@ -1,12 +1,12 @@
 package routers
 
 import (
-	"github.com/RomanenkoDR/metrics/internal/config/server"
+	"github.com/RomanenkoDR/metrics/internal/config/server/types"
 	"github.com/RomanenkoDR/metrics/internal/handlers"
 	"github.com/go-chi/chi/v5"
 )
 
-func InitRouter(cfg server.Options, h handlers.Handler) (chi.Router, error) {
+func InitRouter(cfg types.Options, h handlers.Handler) (chi.Router, error) {
 	router := chi.NewRouter()
 
 	setupMiddleware(router, cfg)
