837348e405c84f549c203850430e891917d742e2
Dmitry Romanenko <letisflywithme@icloud.com>
2025-01-10 10:03:25 +0300

    fix import

--- internal/routers/setup_middleware.go
+++ internal/routers/setup_middleware.go
@@ -1,14 +1,14 @@
 package routers
 
 import (
-	"github.com/RomanenkoDR/metrics/internal/config/server"
+	"github.com/RomanenkoDR/metrics/internal/config/server/types"
 	"github.com/RomanenkoDR/metrics/internal/middleware/gzip"
 	"github.com/RomanenkoDR/metrics/internal/middleware/logger"
 	"github.com/RomanenkoDR/metrics/internal/middleware/token"
 	"github.com/go-chi/chi/v5"
 )
 
-func setupMiddleware(router chi.Router, cfg server.Options) {
+func setupMiddleware(router chi.Router, cfg types.Options) {
 	router.Use(logger.LogHandler)
 	router.Use(gzip.GzipHandle)
 	if cfg.Key != "" {
