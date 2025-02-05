5282fd33c8b92593293bf7131187aa7ea8eb3284
Dmitry Romanenko <letisflywithme@icloud.com>
2025-02-05 23:23:48 +0300

    add build setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/RomanenkoDR/metrics/internal/config/server"
+import (
+	"fmt"
+	"github.com/RomanenkoDR/metrics/internal/config/server"
+)
 import _ "net/http/pprof"
 
+var (
+	buildVersion = "N/A" // VERSION 1-курс, 8-спринт, 20-инкремент
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	server.Run()
 }
